41e6df9b74f8b7184377bdbd18e1bfa9c8019e1a
Troy Allen <165961938+troycallen@users.noreply.github.com>
2024-06-08 19:36:02 -0400

    Update decision tree.py

--- decision tree.py
+++ decision tree.py
@@ -19,3 +19,9 @@ new_ratings = model.predict(X_new)
 print(f"Predicted Ratings: {new_ratings}")
 # df2['Ratings'] = new_ratings
 # df2.to_csv('DT_Ratings.csv')
+
+# Decision Tree with cross-validation
+dt = DecisionTreeClassifier(random_state=42)
+dt_scores = cross_val_score(dt, X, y, cv=3)  # 2-fold cross-validation
+dt_mean_score = dt_scores.mean()
+print(f"Decision Tree mean accuracy: {dt_mean_score:.2f}")
